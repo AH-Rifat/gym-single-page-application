0df44bb4294b64b1b5583a89dab398d6cb20035a
ah-rifat <rifat266@gmail.com>
2022-08-13 16:58:27 +0600

    delete funtion add in modal and Pagination

--- app/Http/Livewire/PaymentList.php
+++ app/Http/Livewire/PaymentList.php
@@ -4,15 +4,33 @@ namespace App\Http\Livewire;
 
 use App\Models\Payment;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class PaymentList extends Component
 {
     public $showFilter = false;
-    public $year, $selectDate;
+    public $year, $selectDate, $getId, $paginateValue;
+    use WithPagination;
+    protected $paginationTheme = 'bootstrap';
 
     public function render()
     {
-        $data = Payment::whereYear('payment_date',$this->year)->orWhere('payment_date',$this->selectDate)->get();
+        $data = Payment::whereYear('payment_date', $this->year)
+            ->orWhere('payment_date', $this->selectDate)
+            ->orderBy('id', "DESC")
+            ->paginate($this->paginateValue);
         return view('livewire.payment-list', compact('data'));
+    }
+
+    public function getDeleteId($var)
+    {
+        $this->getId = $var;
+    }
+
+    public function deletePayment()
+    {
+        Payment::where('id', $this->getId)->delete();
+        $this->emit('closeModel');
+        session()->flash('danger', 'Successfully Deleted');
     }
 }
--- resources/views/livewire/payment-list.blade.php
+++ resources/views/livewire/payment-list.blade.php
@@ -1,4 +1,5 @@
 <div>
+    @include('toastrMgs')
     <div class="row">
         <div class="col-xl-12">
             <div class="card">
@@ -8,12 +9,19 @@
                             <input type="checkbox" wire:model="showFilter" class="btn-check" id="filter-btn-check"
                                 checked="" autocomplete="off">
                             <label class="btn btn-primary" for="filter-btn-check"><i
-                                    class='bx bx-filter-alt me-2'></i>Filter</label>
+                                    class='bx bx-filter-alt me-2'></i>Filter {{$paginateValue}}</label>
                         </div>
                         <div class="d-flex align-self-end">
                             @if ($showFilter == true)
                                 <input class="form-control me-2" type="date" wire:model.prevent="selectDate">
-                                <input class="form-control" placeholder="Year" type="text" wire:model.debounce.1000ms="year">
+                                <input class="form-control" placeholder="Year" type="text"
+                                    wire:model.debounce.1000ms="year">
+                                    <select class="form-select w-25 ms-1" wire:model.prevent="paginateValue">
+                                        <option value="10" selected>10</option>
+                                        <option value="20">20</option>
+                                        <option value="50">50</option>
+                                        <option value="100">100</option>
+                                    </select>
                             @endif
                         </div>
                     </div>
@@ -26,7 +34,7 @@
                                 <th>Member Name</th>
                                 <th>Amount</th>
                                 <th>Date</th>
-                                {{-- <th>Actions</th> --}}
+                                <th>Actions</th>
                             </tr>
                         </thead>
                         <tbody class="table-border-bottom-0">
@@ -35,7 +43,12 @@
                                     <td>{{ $item->member_id }}</td>
                                     <td>{{ $item->memberTable->name }}</td>
                                     <td>{{ $item->amount }}</td>
-                                    <td>{{ $item->payment_date }}</td>
+                                    <td>{{ $item->payment_date->format('Y-m-d') }}</td>
+                                    <td><button class="btn btn-icon btn-danger" data-bs-toggle="modal"
+                                            data-bs-target="#deleteModal"
+                                            wire:click.lazy="getDeleteId({{ $item->id }})">
+                                            <span class="tf-icons bx bx-trash"></span>
+                                        </button></td>
                                     {{-- <td>
                                         <div class="dropdown">
                                             <button type="button" class="btn p-0 dropdown-toggle hide-arrow"
@@ -59,8 +72,37 @@
                             @endforelse
                         </tbody>
                     </table>
+                    <div class="m-4">
+                        <hr>
+                        {{ $data->links() }}
+                    </div>
                 </div>
             </div>
+        </div>
+    </div>
+
+    {{-- Delete Modal --}}
+    <div wire:ignore.self class="modal fade" id="deleteModal" data-bs-backdrop="static" tabindex="-1">
+        <div class="modal-dialog">
+            <form class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="backDropModalTitle"></h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <div class="row">
+                        <div class="col mb-3">
+                            <h2>Are you Sure ?</h2>
+                        </div>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-outline-secondary" data-bs-dismiss="modal">
+                        Close
+                    </button>
+                    <button type="button" wire:click.prevent="deletePayment" class="btn btn-danger">Delete</button>
+                </div>
+            </form>
         </div>
     </div>
 </div>
